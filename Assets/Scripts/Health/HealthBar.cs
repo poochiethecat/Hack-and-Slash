94ffbc34cee8f48621228625c9d813c63e3eb8d9
Gabriel Nadler <nadlergabriel@gmail.com>
2013-06-21 18:37:51 +0200

    Moved functions around in the healthbar trying to group them according to what they do.

--- Assets/Scripts/Health/HealthBar.cs
+++ Assets/Scripts/Health/HealthBar.cs
@@ -81,6 +81,8 @@ public abstract class HealthBar {
     private Func<float> currentHealth;
 
 
+    // Getters for computed variables
+
     private float CurrentHealth {
         get {
             float health = currentHealth();
@@ -112,28 +114,54 @@ public abstract class HealthBar {
     }
 
 
-    private Boolean isConfigured()
-    {
-        bool m =  this.maxHealth == null;
-        bool c =  this.currentHealth == null;
-        bool t = this._transform == null;
-
-        if (m&&c&&t)
-        {
-            Debug.LogWarning("This HealthBar has not been configured.");
-            return false;
-        }
-
-        if (m) Debug.LogWarning("maxHealth has not been configured on this healthbar.");
-        if (c) Debug.LogWarning("current has not been configured on this healthbar.");
-        if (t) Debug.LogWarning("transform has not been configured on this healthbar.");
-        return !m && !c && !t;
-    }
 
 
-
+    // Public Functions
     public abstract void OnGUI();
 
+    /// <summary>
+    /// Draws this healthbar on screen. Only draws if this healthbar has been configured.
+    /// </summary>
+    /// <param name="text">String: The text to draw onto the screen</param>
+    /// <param name="drawDescription">Boolean(true)Should the Descriptiontext be drawn</param>
+    /// <param name="noText">Boolean(false)Should any text be drawn</param>
+    public void draw(string text, bool drawDescription = true, bool noText = false)
+    {
+        if (this.Configured)
+        {
+            if (this.firstrun)
+            {
+                // GUI-Functions can only be called during GUI calls, so we call it in here.
+                this.initGUI();
+                this.firstrun = false;
+            }
+            this.healthBox.width  = this.backgroundBox.width*this.HealthPercentage;
+            this.update();
+
+
+
+            GUI.Box(this.borderBox,"",this.borderStyle);
+            GUI.Box(this.backgroundBox,"",this.backgroundStyle);
+            GUI.Box(this.healthBox,"", this.healthBarStyle);
+            if (drawDescription)
+            {
+                text = text + this.CurrentHealth + "/" + this.MaximumHealth;
+            } else if (noText)
+            {
+                text = "";
+            } else
+            {
+                text = this.CurrentHealth + "/" + this.MaximumHealth;
+            }
+            if (State.getState (this._transform).TargetState == StateName.Targetted)
+            {
+                GUI.Box(this.backgroundBox,text,this.textStyleTargetted);
+            } else
+            {
+                GUI.Box(this.backgroundBox,text,this.textStyleNormal);
+            }
+        }
+    }
     /// <summary>
     /// This Method MUST be called before any use of a healthbar, it will not do anything.
     /// </summary>
@@ -148,6 +176,15 @@ public abstract class HealthBar {
         this.firstrun = true;
     }
 
+
+    // Initialization
+
+    protected void initGUI()
+    {
+        this.initTextures();
+        this.createStyles();
+    }
+
     private void initTextures()
     {
         this.backgroundTexture = ColoredTexture.generatePixel(this.background);
@@ -155,66 +192,6 @@ public abstract class HealthBar {
         this.clearTexture = ColoredTexture.generatePixel(Color.clear);
     }
 
-
-
-    // Returns a Texture with a color based on the current Health of the entity.
-    Texture2D healthTexture()
-    {
-
-        // Use LAB-Colors to find a linear path between any two colors in the human colorspace.
-        LABColor _fullHealth = new LABColor(this._lastFullHealth);
-        LABColor _critHealth = new LABColor(this._lastCriticalHealth);
-
-        float healthPercentage = this.HealthPercentage;
-
-
-        // Reach 0 a bit faster than standard
-        if (healthPercentage < cutoffPercentage)
-            healthPercentage = 0;
-        double newLightness = _critHealth.l - healthPercentage*(_critHealth.l - _fullHealth.l);
-        double newA = _critHealth.a -healthPercentage*(_critHealth.a - _fullHealth.a);
-        double newB = _critHealth.b - healthPercentage*(_critHealth.b - _fullHealth.b);
-        LABColor targetColor = new LABColor((float)newLightness, (float)newA, (float)newB);
-        // Debug.Log(targetColor.ToColor());
-        return ColoredTexture.generatePixel(targetColor.ToColor());
-    }
-
-    private bool colorsEqual(Color a, Color b)
-    {
-        bool cr = Mathf.Approximately(a.r, b.r);
-        bool cg = Mathf.Approximately(a.g, b.g);
-        bool cb = Mathf.Approximately(a.b, b.b);
-        bool ca = Mathf.Approximately(a.a, b.a);
-        bool complete = cr && cg && cb && ca;
-        bool direct = Mathf.Approximately(a.r, b.r) && Mathf.Approximately(a.g, b.g) && Mathf.Approximately(a.b, b.g) && Mathf.Approximately(a.a,b.a);
-        return direct;
-        //return
-    }
-
-    private void updateTextures(bool forceHealth = false)
-    {
-        if (this.backgroundTexture == null || !this.colorsEqual(this.background, this._lastBackground))
-        {
-            this._lastBackground = this.background;
-            this.backgroundTexture = ColoredTexture.generatePixel(this._lastBackground);
-        }
-        if (this.borderTexture == null || !this.colorsEqual(this.border, this._lastBackground))
-        {
-            this._lastBorder = this.border;
-            this.borderTexture = ColoredTexture.generatePixel(this._lastBorder);
-        }
-        bool criticalHealthEqual = this.colorsEqual(this.criticalHealth,this._lastCriticalHealth);
-        bool fullHealthEqual = this.colorsEqual(this.fullHealth,this._lastFullHealth);
-        if (!fullHealthEqual || this._lastHealthTexture == null)
-            this._lastFullHealth = this.fullHealth;
-        if (!criticalHealthEqual || this._lastHealthTexture == null)
-            this._lastCriticalHealth = this.criticalHealth;
-        if (forceHealth || !fullHealthEqual || !criticalHealthEqual)
-            this._lastHealthTexture = this.healthTexture();
-    }
-
-
-    // Creates the styles we need
     private void createStyles()
     {
         this.backgroundStyle = new GUIStyle();
@@ -244,67 +221,102 @@ public abstract class HealthBar {
 
     }
 
-    private void updateStyles()
+
+    // Helper Functions
+
+    private Boolean isConfigured()
+    {
+        bool m =  this.maxHealth == null;
+        bool c =  this.currentHealth == null;
+        bool t = this._transform == null;
+
+        if (m&&c&&t)
+        {
+            Debug.LogWarning("This HealthBar has not been configured.");
+            return false;
+        }
+
+        if (m) Debug.LogWarning("maxHealth has not been configured on this healthbar.");
+        if (c) Debug.LogWarning("current has not been configured on this healthbar.");
+        if (t) Debug.LogWarning("transform has not been configured on this healthbar.");
+        return !m && !c && !t;
+    }
+
+    private bool colorsEqual(Color a, Color b)
+    {
+        bool cr = Mathf.Approximately(a.r, b.r);
+        bool cg = Mathf.Approximately(a.g, b.g);
+        bool cb = Mathf.Approximately(a.b, b.b);
+        bool ca = Mathf.Approximately(a.a, b.a);
+        bool complete = cr && cg && cb && ca;
+        bool direct = Mathf.Approximately(a.r, b.r) && Mathf.Approximately(a.g, b.g) && Mathf.Approximately(a.b, b.g) && Mathf.Approximately(a.a,b.a);
+        return direct;
+        //return
+    }
+
+    // Returns a 1x1 Texture with a color based on the current Health of the entity.
+    Texture2D healthTexture()
+    {
+
+        // Use LAB-Colors to find a linear path between any two colors in the human colorspace.
+        LABColor _fullHealth = new LABColor(this._lastFullHealth);
+        LABColor _critHealth = new LABColor(this._lastCriticalHealth);
+
+        float healthPercentage = this.HealthPercentage;
+
+
+        // Reach 0 a bit faster than standard
+        if (healthPercentage < cutoffPercentage)
+            healthPercentage = 0;
+        double newLightness = _critHealth.l - healthPercentage*(_critHealth.l - _fullHealth.l);
+        double newA = _critHealth.a -healthPercentage*(_critHealth.a - _fullHealth.a);
+        double newB = _critHealth.b - healthPercentage*(_critHealth.b - _fullHealth.b);
+        LABColor targetColor = new LABColor((float)newLightness, (float)newA, (float)newB);
+        // Debug.Log(targetColor.ToColor());
+        return ColoredTexture.generatePixel(targetColor.ToColor());
+    }
+
+    // Update Functions
+
+    private void update()
     {
         this.updateTextures();
+        this.updateStyles();
+    }
+
+    private void updateStyles()
+    {
         this.healthBarStyle.normal.background = this._lastHealthTexture;
         this.borderStyle.normal.background = this.borderTexture;
         this.backgroundStyle.normal.background = this.backgroundTexture;
     }
 
 
-
-    protected void initGUI()
+    private void updateTextures(bool forceHealth = false)
     {
-        if (this.firstrun)
+        if (this.backgroundTexture == null || !this.colorsEqual(this.background, this._lastBackground))
         {
-            this.initTextures();
-            this.createStyles();
-            this.firstrun = false;
-
+            this._lastBackground = this.background;
+            this.backgroundTexture = ColoredTexture.generatePixel(this._lastBackground);
         }
+        if (this.borderTexture == null || !this.colorsEqual(this.border, this._lastBackground))
+        {
+            this._lastBorder = this.border;
+            this.borderTexture = ColoredTexture.generatePixel(this._lastBorder);
+        }
+        bool criticalHealthEqual = this.colorsEqual(this.criticalHealth,this._lastCriticalHealth);
+        bool fullHealthEqual = this.colorsEqual(this.fullHealth,this._lastFullHealth);
+        if (!fullHealthEqual || this._lastHealthTexture == null)
+            this._lastFullHealth = this.fullHealth;
+        if (!criticalHealthEqual || this._lastHealthTexture == null)
+            this._lastCriticalHealth = this.criticalHealth;
+        if (forceHealth || !fullHealthEqual || !criticalHealthEqual)
+            this._lastHealthTexture = this.healthTexture();
     }
 
 
 
-    /// <summary>
-    /// Draws this healthbar on screen. Only draws if this healthbar has been configured.
-    /// </summary>
-    /// <param name="text">String: The text to draw onto the screen</param>
-    /// <param name="drawDescription">Boolean(true)Should the Descriptiontext be drawn</param>
-    /// <param name="noText">Boolean(false)Should any text be drawn</param>
-    public void draw(string text, bool drawDescription = true, bool noText = false)
-    {
-        if (this.Configured)
-        {
-            this.initGUI();
-            this.healthBox.width  = this.backgroundBox.width*this.HealthPercentage;
-            this.updateStyles();
 
 
-
-            GUI.Box(this.borderBox,"",this.borderStyle);
-            GUI.Box(this.backgroundBox,"",this.backgroundStyle);
-            GUI.Box(this.healthBox,"", this.healthBarStyle);
-            if (drawDescription)
-            {
-                text = text + this.CurrentHealth + "/" + this.MaximumHealth;
-            } else if (noText)
-            {
-                text = "";
-            } else
-            {
-                text = this.currentHealth() + "/" + this.maxHealth();
-            }
-            if (State.getState (this._transform).TargetState == StateName.Targetted)
-            {
-                GUI.Box(this.backgroundBox,text,this.textStyleTargetted);
-            } else
-            {
-                GUI.Box(this.backgroundBox,text,this.textStyleNormal);
-            }
-        }
-
-    }
 }
 
